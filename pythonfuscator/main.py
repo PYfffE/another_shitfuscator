cac96188089b5037bc41dff867e4ab880c7ed1c0
pyfffe <pyfffe@gmail.com>
2024-05-25 09:47:48 +0300

    add test payload for ossets check

--- pythonfuscator/main.py
+++ pythonfuscator/main.py
@@ -2,6 +2,8 @@
 
 import shell_bytes
 
+# Choose real or test payload
+# shellcode = shell_bytes.ShellBytes('./resources/instructions_hex.txt')
 shellcode = shell_bytes.ShellBytes('./resources/instructions_hex.txt')
 
 # shellcode.assemble('test.raw')
@@ -10,18 +12,18 @@ print('БЫЛО')
 print('\n'.join(shellcode.get_payload()))
 print()
 
-shellcode.expand_offsets()
+# shellcode.expand_offsets()
 
 # shellcode.add_nops_after_each_instruction()
 # shellcode.add_instruction('90', 10)
 
 # shellcode.add_garbage_instructions_after_each_instruction('83 c0 46', '83 e8 46')
 
-shellcode.add_garbage_instructions_after_each_instruction_with_jmp()
+# shellcode.add_garbage_instructions_after_each_instruction_with_jmp()
 
 
-shellcode.add_prefix(['90', '90', '90'])
-shellcode.add_postfix(['90', '90', '90'])
+# shellcode.add_prefix(['90', '90', '90'])
+# shellcode.add_postfix(['90', '90', '90'])
 
 print('СТАЛО')
 print('\n'.join(shellcode.get_payload()))
